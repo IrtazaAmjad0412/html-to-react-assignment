3bebafd543c9ff60d75da0a8fc6209c73dcdde7b
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-10-15 23:38:18 -0400

    Changed component from func to class

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,21 +1,25 @@
+import React from "react";
 import "./CharacterList.css";
 
-export const CharacterList = ({ data }) => {
-  return (
-    <section id="character-cards">
-      {data.map((char, index) => {
-        const { name, nickName, imageUrl, background } = char;
-        return (
-          <div className="card" key={index}>
-            <div className="card-titles">
-              <h3>{name}</h3>
-              {nickName && <h4>{nickName}</h4>}
+export class CharacterList extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <section id="character-cards">
+        {data.map((char, index) => {
+          const { name, nickName, imageUrl, background } = char;
+          return (
+            <div className="card" key={index}>
+              <div className="card-titles">
+                <h3>{name}</h3>
+                {nickName && <h4>{nickName}</h4>}
+              </div>
+              <img src={imageUrl} alt="" />
+              <p>{background}</p>
             </div>
-            <img src={imageUrl} alt="" />
-            <p>{background}</p>
-          </div>
-        );
-      })}
-    </section>
-  );
-};
+          );
+        })}
+      </section>
+    );
+  }
+}
